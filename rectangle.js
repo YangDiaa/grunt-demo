a106b752d74fe5578cc202a6f110e73862e793fa
YangDiaa <736208296@qq.com>
2020-02-28 10:22:09 +0800

    Grunt Mocha 前端代码单元测试 grunt-mocha

---
+++ rectangle.js
@@ -0,0 +1,27 @@
+function roundFractional(x, n) {
+    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+}
+$(function(){
+    var $width = $('#width'),
+        $height = $('#height'),
+        $btnCal = $('#calculate'),
+        $perimeter = $('#perimeter'),
+        $area = $('#area');
+    /*calc button click event*/
+    $btnCal.click(function(){
+        //get value
+        var w = Number($width.val()),
+            h = Number($height.val());
+
+        //calculate
+        // var p = roundFractional(w*2 + h*2, 4);
+        //     a = roundFractional(w * h, 10);
+
+        //output
+        // $perimeter.val(p);
+        // $area.val(a);
+        var rect = rectangle();
+        $perimeter.val(rect.perimeter(w,h));
+        $area.val(rect.area(w,h));
+    })
+});
