02333cfac7ca066bc97d0d61418a36e84de30e33
YangDiaa <736208296@qq.com>
2020-03-04 08:50:10 +0800

    Grunt 图片合并任务 grunt-spritesmith

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sprite: {
+      all: {
+        src: 'images/*.png',
+        dest: 'dist/bundle.png',
+        destCss: 'dist/sprite.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-spritesmith');
+
+  grunt.registerTask('default', ['sprite']);
+};
